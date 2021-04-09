7cc128144693a3873c9ec3e2fcf571b502d4ebfa
Akash Konnur <akash.m.konnur@gmail.com>
2021-04-09 12:45:04 +0530

    la added

--- src/main/java/com/example/demo/filter/JwtFilter.java
+++ src/main/java/com/example/demo/filter/JwtFilter.java
@@ -16,7 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-//important
+//important la
 @Component
 
 public class JwtFilter extends OncePerRequestFilter {
