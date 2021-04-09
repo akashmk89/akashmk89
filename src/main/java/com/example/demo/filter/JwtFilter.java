a71d99e0fc1b5f6de76985dcbde56e13cf2b87a0
Akash Konnur <akash.m.konnur@gmail.com>
2021-04-09 12:49:59 +0530

    la added

--- src/main/java/com/example/demo/filter/JwtFilter.java
+++ src/main/java/com/example/demo/filter/JwtFilter.java
@@ -17,6 +17,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 //important la
+
+// branch 1 commit
+
 @Component
 
 public class JwtFilter extends OncePerRequestFilter {
